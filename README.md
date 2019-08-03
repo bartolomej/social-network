e64ea1c25d181e6e0533dddac64a1fdd1763754b
bartolomej <bartolomej.kozorog@gmail.com>
2019-08-03 23:17:37 +0200

    Scraper extracts first 40 followers and user profile details.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# instagram-grapj
-Instagram social network graph.
+# Instagram social network
+
+
+#### Resources:
+1. Neo4j js driver (https://neo4j.com/docs/api/javascript-driver/current/)
+2. cheerio resources (http://zetcode.com/javascript/cheerio/)
+3. puppeteer scroll (https://stackoverflow.com/questions/51529332/puppeteer-scroll-down-until-you-cant-anymore)
---
+++ scrape.js
@@ -0,0 +1,58 @@
+const puppeteer = require('puppeteer');
+const $ = require('cheerio');
+
+module.exports.scrapeProfileHtml = async (username, password, user) => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+
+  await page.goto('https://www.instagram.com/accounts/login/');
+  await page.waitForSelector('[name=username]', {visible: true});
+
+  await page.type('[name=username]', username);
+  await page.type('[name=password]', password);
+
+  await page.click('[type=submit]');
+  await page.waitFor(5000);
+  await page.goto(`https://www.instagram.com/${user}`);
+  await page.waitForSelector('img', {visible: true});
+
+  await page.screenshot({path: `${user}.png`});
+
+  const details = await page.evaluate(() => {
+    const details = document.getElementsByClassName('Y8-fY ');
+
+    return Array.from(details).map(ele => ele.innerHTML);
+  });
+
+  await page.evaluate(() => {
+    document.getElementsByClassName('-nal3 ')[1].click()
+  });
+  await page.waitFor(1000);
+
+  const followers = await page.evaluate(() => {
+    const list = document.getElementsByClassName('_0mzm- ZUqME');
+    return Array.from(list).map(ele => ele.innerHTML);
+  });
+
+  await browser.close();
+
+  return {details, followers};
+};
+
+
+module.exports.extractProfileData = (detailsHtml, followersHtml) => {
+  let profileDetails = detailsHtml.map(node => $('.g47SY ', node).text());
+  let followers = followersHtml.map(node => ({
+    username: $('.xLCgt.MMzan.KV-D4', node).text(),
+    img: $('._6q-tv', node).attr('src')
+  }));
+
+  return {
+    details: {
+      posts: profileDetails[0],
+      followers: profileDetails[1],
+      following: profileDetails[2]
+    },
+    followers
+  };
+};
